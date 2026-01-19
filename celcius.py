566091b5d27fa251cf5a4a81d89027766848d261
Harsha <01fe24bca217@kletech.ac.in>
2026-01-19 09:51:37 +0530

    Celcius Commit

---
+++ celcius.py
@@ -0,0 +1,9 @@
+def celcius_to_fahrenheit(c):
+    return(c*9/5)+32
+def main():
+    c=float(int(input("Enter Celcius:35")))
+    print(celcius_to_fahrenheit(c))
+    
+if __name__=="__main__":
+    main()
+    
